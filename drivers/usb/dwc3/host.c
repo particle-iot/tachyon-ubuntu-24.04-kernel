2bda15e2048b4f3c101340794c43b03094ab1000
Ran Wang <ran.wang_1@nxp.com>
2024-01-24 10:25:25 -0500

    usb: dwc3: Add workaround for host mode VBUS glitch when boot
    When DWC3 is set to host mode by programming register DWC3_GCTL, VBUS
    (or its control signal) will be turned on immediately on related Root Hub
    ports. Then, the VBUS is turned off for a little while(15us) when do xhci
    reset (conducted by xhci driver) and back to normal finally, we can
    observe a negative glitch of related signal happen.
    
    This VBUS glitch might cause some USB devices enumeration fail if kernel
    boot with them connected. Such as LS1012AFWRY/LS1043ARDB/LX2160AQDS
    /LS1088ARDB with Kingston 16GB USB2.0/Kingston USB3.0/JetFlash Transcend
    4GB USB2.0 drives. The fail cases include enumerated as full-speed device
    or report wrong device descriptor, etc.
    
    One SW workaround which can fix this is by programing all xhci PORTSC[PP]
    to 0 to turn off VBUS immediately after setting host mode in DWC3 driver
    (per signal measurement result, it will be too late to do it in
    xhci-plat.c or xhci.c). Then, after xhci reset complete in xhci driver,
    PORTSC[PP]s' value will back to 1 automatically and VBUS on at that time,
    no glitch happen and normal enumeration process has no impact.
    
    Acked-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Signed-off-by: Ran Wang <ran.wang_1@nxp.com>
    Reviewed-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Frank Li <Frank.Li@nxp.com>
    Link: https://lore.kernel.org/r/20240124152525.3910311-4-Frank.Li@nxp.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -13,26 +13,51 @@
 #include <linux/usb.h>
 #include <linux/usb/hcd.h>
 
-#include "../host/xhci-plat.h"
+#include "../host/xhci-port.h"
+#include "../host/xhci-ext-caps.h"
+#include "../host/xhci-caps.h"
 #include "core.h"
 
-static void dwc3_xhci_plat_start(struct usb_hcd *hcd)
+#define XHCI_HCSPARAMS1		0x4
+#define XHCI_PORTSC_BASE	0x400
+
+/**
+ * dwc3_power_off_all_roothub_ports - Power off all Root hub ports
+ * @dwc: Pointer to our controller context structure
+ */
+static void dwc3_power_off_all_roothub_ports(struct dwc3 *dwc)
 {
-	struct platform_device *pdev;
-	struct dwc3 *dwc;
+	void __iomem *xhci_regs;
+	u32 op_regs_base;
+	int port_num;
+	u32 offset;
+	u32 reg;
+	int i;
 
-	if (!usb_hcd_is_primary_hcd(hcd))
-		return;
+	/* xhci regs is not mapped yet, do it temperary here */
+	if (dwc->xhci_resources[0].start) {
+		xhci_regs = ioremap(dwc->xhci_resources[0].start, DWC3_XHCI_REGS_END);
+		if (IS_ERR(xhci_regs)) {
+			dev_err(dwc->dev, "Failed to ioremap xhci_regs\n");
+			return;
+		}
 
-	pdev = to_platform_device(hcd->self.controller);
-	dwc = dev_get_drvdata(pdev->dev.parent);
+		op_regs_base = HC_LENGTH(readl(xhci_regs));
+		reg = readl(xhci_regs + XHCI_HCSPARAMS1);
+		port_num = HCS_MAX_PORTS(reg);
 
-	dwc3_enable_susphy(dwc, true);
+		for (i = 1; i <= port_num; i++) {
+			offset = op_regs_base + XHCI_PORTSC_BASE + 0x10 * (i - 1);
+			reg = readl(xhci_regs + offset);
+			reg &= ~PORT_POWER;
+			writel(reg, xhci_regs + offset);
+		}
+
+		iounmap(xhci_regs);
+	} else {
+		dev_err(dwc->dev, "xhci base reg invalid\n");
+	}
 }
-
-static const struct xhci_plat_priv dwc3_xhci_plat_quirk = {
-	.plat_start = dwc3_xhci_plat_start,
-};
 
 static void dwc3_host_fill_xhci_irq_res(struct dwc3 *dwc,
 					int irq, char *name)
@@ -86,6 +111,13 @@ int dwc3_host_init(struct dwc3 *dwc)
 	struct platform_device	*xhci;
 	int			ret, irq;
 	int			prop_idx = 0;
+
+	/*
+	 * Some platforms need to power off all Root hub ports immediately after DWC3 set to host
+	 * mode to avoid VBUS glitch happen when xhci get reset later.
+	 */
+	if (dwc->host_vbus_glitches_quirk)
+		dwc3_power_off_all_roothub_ports(dwc);
 
 	irq = dwc3_host_get_irq(dwc);
 	if (irq < 0)
