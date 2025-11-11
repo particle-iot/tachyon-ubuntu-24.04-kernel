4c3737f7b59995c33468b6bdf8a661b7fa8758c7
Andrey Tolstoy <avtolstoy@gmail.com>
2025-11-11 20:14:59 +0700

    Particle: 6.8.0-1056.57+particle4
    (cherry picked from commit 2bce2716082b0309854a4a703f20ed135a1d160e)

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -24,7 +24,7 @@ export MAKEFLAGS="-j$(nproc)"
 
 debian/rules clean
 debian/rules updateconfigs
-debian/rules binary-particle meta-particle
+debian/rules binary-particle meta-particle binary-indep
 
 mkdir $DIR/debs
 cp ../*.deb $DIR/debs/
