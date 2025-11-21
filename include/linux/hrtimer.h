74e6f3b3d373f4dbc31ff6a2b5a992b7708ed040
Scott Brust <scott.brust@particle.io>
2025-11-21 14:16:00 -0800

    hrtimers: Introduce hrtimer_setup() to replace hrtimer_init()
    To initialize hrtimer, hrtimer_init() needs to be called and also
    hrtimer::function must be set. This is error-prone and awkward to use.
    
    Introduce hrtimer_setup() which does both of these things, so that users of
    hrtimer can be simplified.
    
    The new setup function also has a sanity check for the provided function
    pointer. If NULL, a warning is emitted and a dummy callback installed.
    
    hrtimer_init() will be removed as soon as all of its users have been
    converted to the new function.
    
    Signed-off-by: Nam Cao <namcao@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/5057c1ddbfd4b92033cd93d37fe38e6b069d5ba6.1730386209.git.namcao@linutronix.de
    
    Fix cherry pick merge error

--- include/linux/hrtimer.h
+++ include/linux/hrtimer.h
@@ -337,6 +337,8 @@ extern void hrtimer_init(struct hrtimer *timer, clockid_t which_clock,
 extern void hrtimer_init_sleeper(struct hrtimer_sleeper *sl, clockid_t clock_id,
 				 enum hrtimer_mode mode);
 
+extern void hrtimer_setup(struct hrtimer *timer, enum hrtimer_restart (*function)(struct hrtimer *),
+			  clockid_t clock_id, enum hrtimer_mode mode);
 #ifdef CONFIG_DEBUG_OBJECTS_TIMERS
 extern void hrtimer_init_on_stack(struct hrtimer *timer, clockid_t which_clock,
 				  enum hrtimer_mode mode);
